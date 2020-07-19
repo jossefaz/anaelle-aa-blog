92bbba6bb2a321f4b918fee30aa0dbf1ea245a54
yossefazoulay <azoulayos@protonmail.com>
2020-07-19 11:49:30 +0300

    Updates

---
+++ precache-manifest.5e1d2664951ed66a299dfd9cfab922b1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faba3a1063cd0ea0b330259f6a4aeca4",
+    "url": "/anaelle-aa-blog/index.html"
+  },
+  {
+    "revision": "dce026d2f2544e879146",
+    "url": "/anaelle-aa-blog/static/css/2.3252bf89.chunk.css"
+  },
+  {
+    "revision": "3d4a407ceb5a05fd5a1b",
+    "url": "/anaelle-aa-blog/static/css/main.26aa7c53.chunk.css"
+  },
+  {
+    "revision": "dce026d2f2544e879146",
+    "url": "/anaelle-aa-blog/static/js/2.adad25de.chunk.js"
+  },
+  {
+    "revision": "d77c619ca2e803ff4203c09914a159e3",
+    "url": "/anaelle-aa-blog/static/js/2.adad25de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4a407ceb5a05fd5a1b",
+    "url": "/anaelle-aa-blog/static/js/main.0a47b89b.chunk.js"
+  },
+  {
+    "revision": "bfece1e773ac07476494",
+    "url": "/anaelle-aa-blog/static/js/runtime-main.aa60f647.js"
+  },
+  {
+    "revision": "fe8bed5feea747e0797e24c4e8b26bb2",
+    "url": "/anaelle-aa-blog/static/media/404.fe8bed5f.png"
+  },
+  {
+    "revision": "939d78a0842e638839c6216da3646d48",
+    "url": "/anaelle-aa-blog/static/media/blog-hero.939d78a0.png"
+  },
+  {
+    "revision": "4a8c832994b48ee4bfc599baaae4410e",
+    "url": "/anaelle-aa-blog/static/media/blog-single.4a8c8329.jpg"
+  },
+  {
+    "revision": "9c9a750812bab056b475fb9cc078f51e",
+    "url": "/anaelle-aa-blog/static/media/favicon.9c9a7508.ico"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/anaelle-aa-blog/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/anaelle-aa-blog/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/anaelle-aa-blog/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/anaelle-aa-blog/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/anaelle-aa-blog/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0a8f0dff9d80807d7c29bb251e6dce5b",
+    "url": "/anaelle-aa-blog/static/media/hero-bg.0a8f0dff.jpg"
+  },
+  {
+    "revision": "84287d09b8053c6fa598893b8910786a",
+    "url": "/anaelle-aa-blog/static/media/react.84287d09.svg"
+  },
+  {
+    "revision": "53da834cf982da90647dc32565f82abf",
+    "url": "/anaelle-aa-blog/static/media/service-bg.53da834c.png"
+  },
+  {
+    "revision": "34c6010a2a87fcd287f2680335bc516a",
+    "url": "/anaelle-aa-blog/static/media/slider-1.34c6010a.jpg"
+  },
+  {
+    "revision": "aca54e4d62bf00e7ef808e315e33ff03",
+    "url": "/anaelle-aa-blog/static/media/slider-bg.aca54e4d.jpg"
+  }
+]);
