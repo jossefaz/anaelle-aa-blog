38463f9b8cb2c87a76ed858b9184bc52fcf644e2
yossefazoulay <azoulayos@protonmail.com>
2020-07-19 22:04:27 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Switcher from "./components/Switcher";
 
 /*
  * @ Component Imported
  * */
-import HomeDefault from "./pages/home/HomeDefault";
-import HomeThreeColumn from "./pages/home/HomeThreeColumn";
-import HomeFourColumn from "./pages/home/HomeFourColumn";
-import HomeFiveColumn from "./pages/home/HomeFiveColumn";
-import HomeBlog from "./pages/home/HomeBlog";
-import HomePersonalPortfolio from "./pages/home/HomePersonalPortfolio";
-import HomeFreelancerPortfolio from "./pages/home/HomeFreelancerPortfolio";
+
+import Home from "./pages/home/HomeFourColumn";
 import PortfolioGridTwoColumnPage from "./pages/portfolio/grid/two-column";
 import PortfolioGridThreeColumnPage from "./pages/portfolio/grid/three-column";
 import PortfolioGridFourColumnPage from "./pages/portfolio/grid/four-column";
@@ -25,17 +19,11 @@ import PortfolioMasonryThreeColumnFullWidthPage from "./pages/portfolio/masonry/
 import PortfolioMasonryFourColumnFullWidthPage from "./pages/portfolio/masonry/four-column-fullwidth";
 import PortfolioMasonryFiveColumnFullWidthPage from "./pages/portfolio/masonry/five-column-fullwidth";
 import PortfolioDetailsPage from "./pages/portfolio/details";
-import BlogThreeColumnPage from "./pages/blog/ThreeColumn";
-import BlogTwoColumnLeftSidebarPage from "./pages/blog/TwoColumnLeftSidebar";
-import BlogTwoColumnRightSidebarPage from "./pages/blog/TwoColumnRightSidebar";
-import BlogDetailsRightSidebarPage from "./pages/blog/BlogDetailsRightSidebar";
-import BlogDetailsLeftSidebarPage from "./pages/blog/BlogDetailsLeftSidebar";
 import AboutPage from "./pages/about";
 import ServicePage from "./pages/service";
 import TeamPage from "./pages/team";
 import ContactPage from "./pages/contact";
 import ErrorPage from "./pages/404Error";
-import DemoPage from "./pages/demo";
 
 const App = () => {
   return (
@@ -46,32 +34,12 @@ const App = () => {
           component={ContactPage}
         />
         <Route
-          path={`${process.env.PUBLIC_URL + "/team"}`}
+          path={`${process.env.PUBLIC_URL + "/shop-collection"}`}
           component={TeamPage}
         />
         <Route
           path={`${process.env.PUBLIC_URL + "/service"}`}
           component={ServicePage}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/blog-details-left-sidebar"}`}
-          component={BlogDetailsLeftSidebarPage}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/blog-details"}`}
-          component={BlogDetailsRightSidebarPage}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/blog-two-column-left-sidebar"}`}
-          component={BlogTwoColumnLeftSidebarPage}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/blog-two-column-right-sidebar"}`}
-          component={BlogTwoColumnRightSidebarPage}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/blog"}`}
-          component={BlogThreeColumnPage}
         />
         <Route
           path={`${process.env.PUBLIC_URL + "/portfolio-details"}`}
@@ -141,22 +109,7 @@ const App = () => {
           path={`${process.env.PUBLIC_URL + "/about"}`}
           component={AboutPage}
         />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/home-freelancer-portfolio"}`}
-          component={HomeFreelancerPortfolio}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/home-personal-portfolio"}`}
-          component={HomePersonalPortfolio}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/home-blog"}`}
-          component={HomeBlog}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/"}`}
-          component={HomeFourColumn}
-        />
+        <Route path={`${process.env.PUBLIC_URL + "/"}`} component={Home} />
 
         <Route exact component={ErrorPage} />
       </Switch>
