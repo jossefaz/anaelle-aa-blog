f7dfa6671f2d704ac6dde2a423b84dd90ea9ee8b
yossefazoulay <azoulayos@protonmail.com>
2020-08-08 22:08:12 +0300

    clean App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 /*
@@ -6,18 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
  * */
 
 import Home from "./pages/home/HomeFourColumn";
-import PortfolioGridTwoColumnPage from "./pages/portfolio/grid/two-column";
 import PortfolioGridThreeColumnPage from "./pages/portfolio/grid/three-column";
-import PortfolioGridFourColumnPage from "./pages/portfolio/grid/four-column";
-import PortfolioGridThreeColumnFullWidthPage from "./pages/portfolio/grid/three-column-fullwidth";
-import PortfolioGridFourColumnFullWidthPage from "./pages/portfolio/grid/four-column-fullwidth";
-import PortfolioGridFiveColumnFullWidthPage from "./pages/portfolio/grid/five-column-fullwidth";
-import PortfolioMasonryTwoColumnPage from "./pages/portfolio/masonry/two-column";
-import PortfolioMasonryThreeColumnPage from "./pages/portfolio/masonry/three-column";
-import PortfolioMasonryFourColumnPage from "./pages/portfolio/masonry/four-column";
-import PortfolioMasonryThreeColumnFullWidthPage from "./pages/portfolio/masonry/three-column-fullwidth";
-import PortfolioMasonryFourColumnFullWidthPage from "./pages/portfolio/masonry/four-column-fullwidth";
-import PortfolioMasonryFiveColumnFullWidthPage from "./pages/portfolio/masonry/five-column-fullwidth";
 import PortfolioDetailsPage from "./pages/portfolio/details";
 import AboutPage from "./pages/about";
 import ServicePage from "./pages/service";
@@ -51,66 +40,7 @@ const App = () => {
             return <ErrorPage />;
           }}
         />
-        <Route
-          path={`${
-            process.env.PUBLIC_URL + "/portfolio-masonry-five-column-fullwidth"
-          }`}
-          component={PortfolioMasonryFiveColumnFullWidthPage}
-        />
-        <Route
-          path={`${
-            process.env.PUBLIC_URL + "/portfolio-masonry-four-column-fullwidth"
-          }`}
-          component={PortfolioMasonryFourColumnFullWidthPage}
-        />
-        <Route
-          path={`${
-            process.env.PUBLIC_URL + "/portfolio-masonry-three-column-fullwidth"
-          }`}
-          component={PortfolioMasonryThreeColumnFullWidthPage}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/portfolio-masonry-four-column"}`}
-          component={PortfolioMasonryFourColumnPage}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/portfolio-masonry-three-column"}`}
-          component={PortfolioMasonryThreeColumnPage}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/portfolio-masonry-two-column"}`}
-          component={PortfolioMasonryTwoColumnPage}
-        />
-        <Route
-          path={`${
-            process.env.PUBLIC_URL + "/portfolio-grid-five-column-fullwidth"
-          }`}
-          component={PortfolioGridFiveColumnFullWidthPage}
-        />
-        <Route
-          path={`${
-            process.env.PUBLIC_URL + "/portfolio-grid-four-column-fullwidth"
-          }`}
-          component={PortfolioGridFourColumnFullWidthPage}
-        />
-        <Route
-          path={`${
-            process.env.PUBLIC_URL + "/portfolio-grid-three-column-fullwidth"
-          }`}
-          component={PortfolioGridThreeColumnFullWidthPage}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/portfolio-grid-four-column"}`}
-          component={PortfolioGridFourColumnPage}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/portfolio-grid-three-column"}`}
-          component={PortfolioGridThreeColumnPage}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL + "/portfolio-grid-two-column"}`}
-          component={PortfolioGridTwoColumnPage}
-        />
+
         <Route
           path={`${process.env.PUBLIC_URL + "/about"}`}
           component={AboutPage}
